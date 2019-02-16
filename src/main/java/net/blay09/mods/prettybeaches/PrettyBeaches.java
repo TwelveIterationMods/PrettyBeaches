9cfbdaaf063bcfbdb70ab96115cc726c16611c80
blay09 <blay09@hotmail.de>
2019-02-16 13:51:08 +0100

    Update Forge and move to COMMON config

--- src/main/java/net/blay09/mods/prettybeaches/PrettyBeaches.java
+++ src/main/java/net/blay09/mods/prettybeaches/PrettyBeaches.java
@@ -6,23 +6,18 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(PrettyBeaches.MOD_ID)
 public class PrettyBeaches {
 
     public static final String MOD_ID = "prettybeaches";
 
-    public static Logger logger = LogManager.getLogger(MOD_ID);
-
     private final FloodingHandler floodingHandler = new FloodingHandler();
 
     public PrettyBeaches() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
-        // TODO Awaiting Forge config COMMON fix
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, PrettyBeachesConfig.commonSpec);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, PrettyBeachesConfig.commonSpec);
     }
 
     public void setup(FMLCommonSetupEvent event) {
