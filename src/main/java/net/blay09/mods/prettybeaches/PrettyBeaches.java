8184e0139f1b8c6b731d2f69d35ee43b20b10dcc
blay09 <blay09@hotmail.de>
2019-02-19 13:56:58 +0100

    Fix potential threading issues

--- src/main/java/net/blay09/mods/prettybeaches/PrettyBeaches.java
+++ src/main/java/net/blay09/mods/prettybeaches/PrettyBeaches.java
@@ -4,23 +4,16 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(PrettyBeaches.MOD_ID)
 public class PrettyBeaches {
 
     public static final String MOD_ID = "prettybeaches";
 
-    private final FloodingHandler floodingHandler = new FloodingHandler();
-
     public PrettyBeaches() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, PrettyBeachesConfig.commonSpec);
-    }
 
-    public void setup(FMLCommonSetupEvent event) {
+        FloodingHandler floodingHandler = new FloodingHandler();
         MinecraftForge.EVENT_BUS.register(floodingHandler);
         MinecraftForge.EVENT_BUS.register(new HarvestBlockHandler(floodingHandler));
     }
