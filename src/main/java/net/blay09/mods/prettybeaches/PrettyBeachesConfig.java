f7e2840a1eb3eeabfb53db834a2fd2885123651b
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2020-09-29 14:45:03 +0300

    better min&max configuration & cleaner config

--- src/main/java/net/blay09/mods/prettybeaches/PrettyBeachesConfig.java
+++ src/main/java/net/blay09/mods/prettybeaches/PrettyBeachesConfig.java
@@ -6,17 +6,16 @@ import net.minecraftforge.common.config.Config;
 public class PrettyBeachesConfig {
 	
 	@Config.Name("Water Lake Check Radius")
-    @Config.Comment("The range in blocks to check the water blocks amount in 3 dimensions. Larger numbers mean more realistic but lower performance. Minimum 3 and maximum 10.")
-    public static int waterCheckRadius = 5;
+	@Config.Comment("The range in blocks to check the water blocks amount in 3 dimensions. Larger numbers mean more realistic but lower performance.")
+	@Config.RangeInt(min=3, max=10)
+	public static int waterCheckRadius = 5;
 
-    @Config.Name("Animated Flooding")
-    @Config.Comment("Whether the flooding of adjacent air blocks should be animated or instant.")
-    public static boolean animatedFlooding = true;
-
-    public static void onConfigReload() {
-    	if(waterCheckRadius < 3)
-    		waterCheckRadius=3;
-    	if(waterCheckRadius > 10)
-    		waterCheckRadius=10;
+	@Config.Name("Animated Flooding")
+	@Config.Comment("Whether the flooding of adjacent air blocks should be animated or instant.")
+	public static boolean animatedFlooding = true;
+	
+	public static void onConfigReload() {
+		if(waterCheckRadius < 3) waterCheckRadius=3;
+    	if(waterCheckRadius > 10) waterCheckRadius=10;
     }
 }
