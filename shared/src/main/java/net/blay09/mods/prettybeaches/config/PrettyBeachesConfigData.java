a30d4fbe01ef340780501891ec3efed1ac341a7f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-06-19 10:39:53 +0200

    fix: Fix list config options missing ExpectedType https://github.com/TwelveIterationMods/CraftingTweaks/issues/200

--- shared/src/main/java/net/blay09/mods/prettybeaches/config/PrettyBeachesConfigData.java
+++ shared/src/main/java/net/blay09/mods/prettybeaches/config/PrettyBeachesConfigData.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import net.blay09.mods.balm.api.config.BalmConfigData;
 import net.blay09.mods.balm.api.config.Comment;
 import net.blay09.mods.balm.api.config.Config;
+import net.blay09.mods.balm.api.config.ExpectedType;
 import net.blay09.mods.prettybeaches.PrettyBeaches;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @Config(PrettyBeaches.MOD_ID)
 public class PrettyBeachesConfigData implements BalmConfigData {
 
+    @ExpectedType(String.class)
     @Comment("List of blocks that should be affected by the adjusted water physics. If you want to have all blocks affected, just include \"*\" in the list.")
     public List<String> affectedBlocks = Lists.newArrayList("minecraft:sand");
 
