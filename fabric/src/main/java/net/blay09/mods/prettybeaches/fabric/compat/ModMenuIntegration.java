e1afda0906c427cc2eb19c7c177e3816b6528ef4
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-12-28 20:33:30 +0100

    feat: Update to Minecraft 1.20.4

--- fabric/src/main/java/net/blay09/mods/prettybeaches/fabric/compat/ModMenuIntegration.java
+++ fabric/src/main/java/net/blay09/mods/prettybeaches/fabric/compat/ModMenuIntegration.java
@@ -1,4 +1,4 @@
-package net.blay09.mods.prettybeaches.compat;
+package net.blay09.mods.prettybeaches.fabric.compat;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
