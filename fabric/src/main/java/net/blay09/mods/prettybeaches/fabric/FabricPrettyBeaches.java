708c042295699398e17223fe53cf83c852996e49
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-19 16:13:38 +0200

    feat: Update to Minecraft 1.20.6

--- fabric/src/main/java/net/blay09/mods/prettybeaches/fabric/FabricPrettyBeaches.java
+++ fabric/src/main/java/net/blay09/mods/prettybeaches/fabric/FabricPrettyBeaches.java
@@ -1,12 +1,13 @@
 package net.blay09.mods.prettybeaches.fabric;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.prettybeaches.PrettyBeaches;
 import net.fabricmc.api.ModInitializer;
 
 public class FabricPrettyBeaches implements ModInitializer {
     @Override
     public void onInitialize() {
-        Balm.initialize(PrettyBeaches.MOD_ID, PrettyBeaches::initialize);
+        Balm.initialize(PrettyBeaches.MOD_ID, EmptyLoadContext.INSTANCE, PrettyBeaches::initialize);
     }
 }
--- neoforge/src/main/java/net/blay09/mods/prettybeaches/NeoForgePrettyBeaches.java
+++ neoforge/src/main/java/net/blay09/mods/prettybeaches/NeoForgePrettyBeaches.java
@@ -1,13 +1,16 @@
 package net.blay09.mods.prettybeaches;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.neoforge.NeoForgeLoadContext;
+import net.neoforged.bus.EventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(PrettyBeaches.MOD_ID)
 public class NeoForgePrettyBeaches {
 
-    public NeoForgePrettyBeaches() {
-        Balm.initialize(PrettyBeaches.MOD_ID, PrettyBeaches::initialize);
+    public NeoForgePrettyBeaches(EventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modEventBus);
+        Balm.initialize(PrettyBeaches.MOD_ID, context, PrettyBeaches::initialize);
     }
 
 }
