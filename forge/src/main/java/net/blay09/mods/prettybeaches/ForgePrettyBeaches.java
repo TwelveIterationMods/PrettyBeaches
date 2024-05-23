070fa78ed50b6794ba3c8bee2568f3427dcdcaca
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-23 14:12:47 +0200

    fix(1.20.6): Fix builds on Forge

--- forge/src/main/java/net/blay09/mods/prettybeaches/ForgePrettyBeaches.java
+++ forge/src/main/java/net/blay09/mods/prettybeaches/ForgePrettyBeaches.java
@@ -1,13 +1,14 @@
 package net.blay09.mods.prettybeaches;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(PrettyBeaches.MOD_ID)
 public class ForgePrettyBeaches {
 
     public ForgePrettyBeaches() {
-        Balm.initialize(PrettyBeaches.MOD_ID, PrettyBeaches::initialize);
+        Balm.initialize(PrettyBeaches.MOD_ID, EmptyLoadContext.INSTANCE, PrettyBeaches::initialize);
     }
 
 }
