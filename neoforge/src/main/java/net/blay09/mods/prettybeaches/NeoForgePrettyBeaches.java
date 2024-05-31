fef30e15d0037eff7d0a96693bf9283add88c929
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-31 08:39:39 +0200

    fix: Fix startup crash on NeoForge #29

--- neoforge/src/main/java/net/blay09/mods/prettybeaches/NeoForgePrettyBeaches.java
+++ neoforge/src/main/java/net/blay09/mods/prettybeaches/NeoForgePrettyBeaches.java
@@ -2,13 +2,13 @@ package net.blay09.mods.prettybeaches;
 
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.neoforge.NeoForgeLoadContext;
-import net.neoforged.bus.EventBus;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(PrettyBeaches.MOD_ID)
 public class NeoForgePrettyBeaches {
 
-    public NeoForgePrettyBeaches(EventBus modEventBus) {
+    public NeoForgePrettyBeaches(IEventBus modEventBus) {
         final var context = new NeoForgeLoadContext(modEventBus);
         Balm.initialize(PrettyBeaches.MOD_ID, context, PrettyBeaches::initialize);
     }
