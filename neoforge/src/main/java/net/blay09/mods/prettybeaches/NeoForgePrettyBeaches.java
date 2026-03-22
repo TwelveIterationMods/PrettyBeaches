59d317717a4afc59c07335e7736b0a412be9f890
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 15:33:41 +0100

    feat: Update to Minecraft 26.1

--- neoforge/src/main/java/net/blay09/mods/prettybeaches/NeoForgePrettyBeaches.java
+++ neoforge/src/main/java/net/blay09/mods/prettybeaches/NeoForgePrettyBeaches.java
@@ -3,13 +3,14 @@ package net.blay09.mods.prettybeaches;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(PrettyBeaches.MOD_ID)
 public class NeoForgePrettyBeaches {
 
-    public NeoForgePrettyBeaches(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgePrettyBeaches(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         Balm.initializeMod(PrettyBeaches.MOD_ID, context, PrettyBeaches::initialize);
     }
 
