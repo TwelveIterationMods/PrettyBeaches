8ca2a632a50f723152eb9605b12a3f5e125961be
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-25 10:29:05 +0100

    refactor: Use FabricTagsProvider.BlockTagsProvider instead of creating our own provider

--- fabric/src/main/java/net/blay09/mods/prettybeaches/fabric/datagen/ModBlockTagProvider.java
+++ fabric/src/main/java/net/blay09/mods/prettybeaches/fabric/datagen/ModBlockTagProvider.java
@@ -1,23 +1,21 @@
 package net.blay09.mods.prettybeaches.fabric.datagen;
 
 import net.blay09.mods.prettybeaches.tag.ModBlockTags;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.data.PackOutput;
-import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
 import java.util.concurrent.CompletableFuture;
 
-public class ModBlockTagProvider extends IntrinsicHolderTagsProvider<Block> {
-    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-        super(output, Registries.BLOCK, registriesFuture, (block) -> block.builtInRegistryHolder().key());
+public class ModBlockTagProvider extends FabricTagsProvider.BlockTagsProvider {
+    public ModBlockTagProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider arg) {
-        tag(ModBlockTags.PRESERVE_PRETTY).add(Blocks.SAND);
+        valueLookupBuilder(ModBlockTags.PRESERVE_PRETTY).add(Blocks.SAND);
     }
 
 }
